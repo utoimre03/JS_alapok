be3e41f819f0f9b7273c3582e10586ee74528010
utoimre03 <utoimre03@gmail.com>
2024-03-01 11:46:53 +0100

    elemek formazasa

--- script.js
+++ script.js
@@ -2,14 +2,46 @@ window.addEventListener("load", function () {
     elemekElerese1();
     elemekElerese2();
     elemekElerese3();
+    elemekElerese4();
+    elemekFormazasa1();
+    esemenyKezeles1();
 });
 
 function elemekElerese1() {
-    /**Ide jön az első feladat */
+    /* Ide jön az 1. feladat */
+    const ELEM = document.querySelectorAll("section h2")[0]
+    ELEM.innerHTML
+    console.log(ELEM)
 }
+
 function elemekElerese2() {
-    /**Ide jön az 2.  feladat */
+    /* Ide jön az 2. feladat */
+    const ELEM = document.getElementById("ide")
+    ELEM.innerHTML = "<p>Jó reggelt!</p>"
 }
+
 function elemekElerese3() {
-    /**Ide jön az 3. feladat */
+    /* Ide jön az 3. feladat */
+    const ELEM = document.querySelectorAll(".ide")[0]
+    ELEM.innerHTML = "<p>Jó reggelt!</p>"
 }
+
+function elemekElerese4() {
+    /* Ide jön az 4. feladat */
+    const ELEM = document.getElementsByClassName("lista")[0]
+    let txt = `<ul>`
+    for (let index = 0; index < 5; index++) {
+        randomSzam = Math.floor(Math.random()*20)+10;
+        txt += `<li>${randomSzam}</li>`
+    }
+    txt += "</ul>"
+    ELEM.innerHTML = txt;
+}
+
+function elemekFormazasa1() {
+    /* Ide jön az 5. feladat */
+    const ELEM = document.getElementsByClassName("lista")[0];
+    ELEM.classList.add("formazott")
+}
+
+//function esemenyKezeles1() {}
